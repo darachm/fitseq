421b844f51e8c2b924ef33858b2ca50de12018cc
darachm <4563512-darachm@users.noreply.gitlab.com>
2021-11-30 11:01:24 -0800

    adding a testing script/dataset to provide for more rapid feedback in development

---
+++ testing/test.sh
@@ -0,0 +1,6 @@
+# Run from the base repo directory, so bash testing/test.sh
+singularity exec --bind PyFitSeq:/PyFitSeq ~/.singularity/fitseq-dev.sif \
+    python3 /PyFitSeq/pyfitseq.py -i testing/data/ppiseq_test_counts.csv \
+        -t 0 1 2 3 4 -m 5 --min-step 0.0001 \
+        -o testing/output/test_out \
+        2>&1 > testing/output/test.err
