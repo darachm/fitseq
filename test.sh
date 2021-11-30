5895780b7a75fbc8d389f60201eec3f289763ac1
darachm <4563512-darachm@users.noreply.gitlab.com>
2021-11-30 11:02:50 -0800

    this was moved into testing a few commits back....

--- test.sh
+++
@@ -1,5 +0,0 @@
-singularity exec --bind ./PyFitSeq:/PyFitSeq ~/.singularity/fitseq-dev.sif \
-    python3 /PyFitSeq/pyfitseq.py -i ppiseq_test_counts.csv \
-        -t 0 1 2 3 4 -m 50 --min-step 0.0001 \
-        -o test_out \
-        2>&1 > test.err
