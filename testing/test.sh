ceb8c2dfeafeaf99fc2748e136f65ae709ce7935
darachm <4563512-darachm@users.noreply.gitlab.com>
2021-11-30 13:50:53 -0800

    for testing better for coding debug

--- testing/test.sh
+++ testing/test.sh
@@ -1,6 +1,7 @@
 # Run from the base repo directory, so bash testing/test.sh
 singularity exec --bind PyFitSeq:/PyFitSeq ~/.singularity/fitseq-dev.sif \
     python3 /PyFitSeq/pyfitseq.py -i testing/data/ppiseq_test_counts.csv \
-        -p 6 -t 0 1 2 3 4 -m 5 --min-step 0.0001 \
+        -p 7 -t 0 1 2 3 4 -m 2 --min-step 0.0001 \
+        --fitness_type m \
         -o testing/output/test_out \
-        2>&1 > testing/output/test.err
+        #2> testing/output/test.err
