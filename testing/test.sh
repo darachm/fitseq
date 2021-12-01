f4a8e33b7a5a09deb0be1d06777c6a1ecc058cab
darachm <4563512-darachm@users.noreply.gitlab.com>
2021-12-01 12:43:42 -0800

    looping in some tweaks on testing, for faster debugging testing

--- testing/test.sh
+++ testing/test.sh
@@ -1,7 +1,7 @@
 # Run from the base repo directory, so bash testing/test.sh
 singularity exec --bind PyFitSeq:/PyFitSeq ~/.singularity/fitseq-dev.sif \
     python3 /PyFitSeq/pyfitseq.py -i testing/data/ppiseq_test_counts.csv \
-        -p 7 -t 0 1 2 3 4 -m 2 --min-step 0.0001 \
+        -p 8 -t 0 1 2 3 4 -m 10 --min-step 0.001 \
         --fitness_type m \
         -o testing/output/test_out \
         #2> testing/output/test.err
