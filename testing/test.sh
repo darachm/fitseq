2c9c725a20c3ae674b3ab5b40e543467e7b67823
darachm <4563512-darachm@users.noreply.gitlab.com>
2021-12-01 19:07:30 -0800

    just testing further out for stability between the likelihood functions - whups mismatch

--- testing/test.sh
+++ testing/test.sh
@@ -1,7 +1,8 @@
 # Run from the base repo directory, so bash testing/test.sh
 singularity exec --bind PyFitSeq:/PyFitSeq ~/.singularity/fitseq-dev.sif \
-    python3 /PyFitSeq/pyfitseq.py -i testing/data/ppiseq_test_counts.csv \
-        -p 8 -t 0 1 2 3 4 -m 10 --min-step 0.001 \
+    python3 /PyFitSeq/pyfitseq.py -i testing/data/ppiseq_test_counts_100.csv \
+        -p 8 -t 0 1 2 3 4 -m 100 --min-step 0.0000 \
+        --min-iter 100 \
         --fitness_type m \
         -o testing/output/test_out \
         #2> testing/output/test.err
