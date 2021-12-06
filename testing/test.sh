c46d9d187c79e244aba17628207f53441371fe86
darachm <4563512-darachm@users.noreply.gitlab.com>
2021-12-06 15:08:49 -0800

    just the latest testing script

--- testing/test.sh
+++ testing/test.sh
@@ -3,6 +3,7 @@ singularity exec --bind PyFitSeq:/PyFitSeq ~/.singularity/fitseq-dev.sif \
     python3 /PyFitSeq/pyfitseq.py -i testing/data/ppiseq_test_counts_1000.csv \
         -p 8 -t 0 1 2 3 4 -m 100 --min-step 0.001 \
         --min-iter 1 \
+        --gtol 1e-2 \
         --max-chunk-size 100000 \
         --fitness_type m \
         --output-mean-fitness testing/output/test_means.csv \
