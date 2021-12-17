ba240ec17ca8a7938388d33f0620cf5fb6b5c7ba
darachm <4563512-darachm@users.noreply.gitlab.com>
2021-12-17 15:40:31 -0800

    adding in testing updates, test file, for debugging andn testing that options are working right

--- testing/test.sh
+++ testing/test.sh
@@ -1,11 +1,8 @@
 # Run from the base repo directory, so bash testing/test.sh
 singularity exec --bind PyFitSeq:/PyFitSeq ~/.singularity/fitseq-dev.sif \
     python3 /PyFitSeq/pyfitseq.py -i testing/data/ppiseq_test_counts_1000.csv \
-        -p 8 -t 0 1 2 3 4 -m 100 --min-step 0.001 \
-        --min-iter 1 \
-        --gtol 1e-2 \
-        --max-chunk-size 100000 \
-        --fitness_type m \
+        -p 8 -t 0 1 2 3 4 \
+        -m 20 --min-step 0.001 \
         --output-mean-fitness testing/output/test_means.csv \
         -o testing/output/test_out.csv
         #2> testing/output/test.err
