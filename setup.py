340194a359f121bf4a535db0a577e767f3279445
darachm <4563512-darachm@users.noreply.gitlab.com>
2021-12-27 16:18:32 -0800

    up on pypi now

--- setup.py
+++ setup.py
@@ -5,18 +5,23 @@ with open('README.md','r') as fh:
 
 setup(
     name='fitseq',
-    version='1.5.0',
+    version='v1.5.0',
+    author='Fangfei Li, Darach Miller',
+    url='https://github.com/darachm/fitseq',
     description=(
         'A utility for fitting lineage fitnesses within a pooled competition '
         'experiment, achieved by iteratively optimizing models of '
         'individual and population-average lineage fitness.'
         ),
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+        ],
+    python_requires=">=3.6",
     packages=['fitseq'],
     scripts=['fitseq/evo_simulator.py','fitseq/fitseq.py'],
     install_requires=['numpy>=1.17.3','pandas>=0.25.3','scipy>=1.3.1','tqdm'],
-    license='MIT',
-    long_description=long_description,
-    long_description_content_type='text/markdown'
+    license='MIT'
     )
